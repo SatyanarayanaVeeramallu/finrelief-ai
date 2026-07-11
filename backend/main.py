54022edae2d1aef0b32ec482c8b85854c2110fe3
SatyanarayanaVeeramallu <satyaacet3@gmail.com>
2026-07-11 09:42:42 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -13,11 +13,8 @@ from app.models.loan_model import Loan
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
